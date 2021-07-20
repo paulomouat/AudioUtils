6d9bf82df59e328d346c1d1af10558b3968e2b49
Paulo Mouat <paulo.mouat@gmail.com>
2021-07-20 15:36:19 -0400

    Added ability to extend markers and regions by a parameterized factor.

--- extend-length-between-markers.py
+++ extend-length-between-markers.py
@@ -6,15 +6,16 @@ from wavfile import read, write
 from wavfileinfo import outputinfo
 
 def usage():
-    print('extend-length-between-markers.py -i <inputfile> -o <outputfile>')
+    print('extend-length-between-markers.py -i <inputfile> -o <outputfile> -f <factor>')
     sys.exit(2)
 
 def main(argv):
     inputfile = ''
     outputfile = ''
+    factoropt = ''
 
     try:
-        opts, args = getopt.getopt(argv, "i:o:", ["inputfile=", "outputfile="])
+        opts, args = getopt.getopt(argv, "i:o:f:", ["inputfile=", "outputfile=", "factor="])
     except getopt.GetoptError:
         usage()
     
@@ -23,11 +24,20 @@ def main(argv):
             inputfile = arg
         elif opt in ("-o", "--outputfile"):
             outputfile = arg
+        elif opt in ("-f", "--factor"):
+            factoropt = arg
 
     if inputfile == '' or outputfile == '':
         usage()
 
-    double = True
+    factor = 1.0
+
+    if factoropt != '':
+        factor = float(factoropt)
+
+    if factor < 1.0:
+        print('the supplied factor needs to be at least 1.0')
+        sys.exit(2)
 
     (rate, data, bits, cue, markers, *other) = read(inputfile, readmarkers=True, readmarkerslist=True)
     print('input file:')
@@ -37,23 +47,22 @@ def main(argv):
     segmentslen = len(segments)
     print('there are', segmentslen, 'segments')
 
-    if not double:
-        sys.exit()
-
     doubleds = []
 
     for idx, segment in enumerate(segments):
-        size = int(segment.size/2)
+        length = segment.shape[0]
+        num_channels = segment.shape[1]
+        delta_length = int(length * factor) - length
         doubleds.append(segment)
-        space = np.zeros_like(segment)
+        space = np.zeros_like(segment, shape=(delta_length, num_channels))
         doubleds.append(space)
 
     doubledmarkers = []
 
     pos = 0
     for idx, doubled in enumerate(doubleds):
-        size = int(doubled.size/2)
-        pos = pos + size
+        length = doubled.shape[0]
+        pos = pos + length
         if idx % 2 == 1 and len(markers) > 0:
             marker = markers[0]
             markers.pop(0)
--- extend-length-between-regions.py
+++ extend-length-between-regions.py
@@ -6,15 +6,16 @@ from wavfile import read, write
 from wavfileinfo import outputinfo
 
 def usage():
-    print('extend-length-between-regions.py -i <inputfile> -o <outputfile>')
+    print('extend-length-between-regions.py -i <inputfile> -o <outputfile> -f <factor>')
     sys.exit(2)
 
 def main(argv):
     inputfile = ''
     outputfile = ''
+    factoropt = ''
 
     try:
-        opts, args = getopt.getopt(argv, "i:o:", ["inputfile=", "outputfile="])
+        opts, args = getopt.getopt(argv, "i:o:f:", ["inputfile=", "outputfile=", "factor="])
     except getopt.GetoptError:
         usage()
     
@@ -23,11 +24,20 @@ def main(argv):
             inputfile = arg
         elif opt in ("-o", "--outputfile"):
             outputfile = arg
+        elif opt in ("-f", "--factor"):
+            factoropt = arg
 
     if inputfile == '' or outputfile == '':
         usage()
 
-    double = True
+    factor = 1.0
+
+    if factoropt != '':
+        factor = float(factoropt)
+
+    if factor < 1.0:
+        print('the supplied factor needs to be at least 1.0')
+        sys.exit(2)
 
     (rate, data, bits, cue, markers, *other) = read(inputfile, readmarkers=True, readmarkerslist=True)
     print('input file:')
@@ -37,29 +47,31 @@ def main(argv):
     segmentslen = len(segments)
     print('there are', segmentslen, 'segments')
 
-    if not double:
-        sys.exit()
-
     doubleds = []
 
     for idx, segment in enumerate(segments):
         doubleds.append(segment)
         if idx > 0:
-            size = int(segment.size/2)
-            prevregion = markers[idx-1]
-            rlength = prevregion['length']
-            space = np.zeros_like(segment)[:size - rlength]
+            length = segment.shape[0]
+            num_channels = segment.shape[1]
+            region = markers[idx-1]
+            rlength = region['length']
+            leftover = length - rlength
+            delta_length = int(leftover * factor) - leftover
+            space = np.zeros_like(segment, shape=(delta_length, num_channels))
             doubleds.append(space)
         else:
-            space = np.zeros_like(segment)
+            length = segment.shape[0]
+            num_channels = segment.shape[1]
+            space = np.zeros_like(segment, shape=(0, num_channels))
             doubleds.append(space)
     
     doubledmarkers = []
 
     pos = 0
     for idx, doubled in enumerate(doubleds):
-        size = int(doubled.size/2)
-        pos = pos + size
+        length = doubled.shape[0]
+        pos = pos + length
         if idx % 2 == 1 and len(markers) > 0:
             marker = markers[0]
             markers.pop(0)
